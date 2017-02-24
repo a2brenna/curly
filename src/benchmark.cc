a5818fed733caa2eb64e8f61df2a7b17a472429a
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2017-02-24 03:35:09 +0000

    Log a todo

--- src/benchmark.cc
+++ src/benchmark.cc
@@ -7,6 +7,7 @@ int main(){
     std::cout << "Starting Benchmark..." << std::endl;
 
     Curl_Instance test("https://csclub.uwaterloo.ca", 16384);
+    //TODO: Test with zero length buffer
 
     for(size_t i = 0; i < 50; i++){
         const auto start_time = std::chrono::high_resolution_clock::now();
